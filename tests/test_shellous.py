b99e1ddd8f4bf21d74fe4e990435dbd5792d6f2d
Bill Fisher <william.w.fisher@gmail.com>
2021-08-22 18:04:00 -0700

    test_broken_pipe_in_failed_pipeline xfails win32

--- tests/test_shellous.py
+++ tests/test_shellous.py
@@ -272,6 +272,7 @@ async def test_broken_pipe_in_pipeline(cat_cmd, echo_cmd):
         await (data | cat_cmd | echo_cmd("abc"))
 
 
+@pytest.mark.xfail(sys.platform == "win32", reason="timeout in wait_closed")
 async def test_broken_pipe_in_failed_pipeline(cat_cmd, echo_cmd):
     "Test broken pipe error within a pipeline; last command fails."
     data = b"c" * PIPE_MAX_SIZE
