0cdb7db3c7d230be5b6c1bc25536eb41586f7b75
Bill Fisher <william.w.fisher@gmail.com>
2021-09-02 20:33:52 -0700

    Remove xfail's.

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -182,27 +182,24 @@ def test_pipeline_call(sh):
         pipe("foo")
 
 
-@pytest.mark.xfail(reason="FIXME")
 def test_invalid_pipeline_override_stdout(sh):
-    """A Pipeline should not override existing stdout redirections."""
+    """A Pipeline will override existing stdout redirections."""
     echo = sh("echo").stdout("/tmp/tmp_file")
-    with pytest.raises(ValueError):
-        echo | sh("grep")
+    pipe = echo | sh("grep")
+    assert pipe == pipeline(sh("echo").stdout("/tmp/tmp_file"), sh("grep"))
 
 
-@pytest.mark.xfail(reason="FIXME")
 def test_invalid_pipeline_override_stdin(sh):
-    """A Pipeline should not override existing stdin redirections."""
+    """A Pipeline will override existing stdin redirections."""
     grep = sh("grep").stdin("/tmp/tmp_file")
-    with pytest.raises(ValueError):
-        sh("echo") | grep
+    pipe = sh("echo") | grep
+    assert pipe == pipeline(sh("echo"), sh("grep").stdin("/tmp/tmp_file"))
 
 
-@pytest.mark.xfail(reason="FIXME")
 def test_invalid_pipeline_operators(sh):
     "Test >> in the middle of a pipeline."
-    with pytest.raises(ValueError):
-        sh("echo") >> "/tmp/tmp_file" | sh("cat")
+    pipe = sh("echo") >> "/tmp/tmp_file" | sh("cat")
+    assert pipe == pipeline(sh("echo").stdout("/tmp/tmp_file", append=True), sh("cat"))
 
 
 def test_pipeline_redirect_stringio(sh):
