45f60d0856387454c9d4751ab6673a2ad4de58e2
Bill Fisher <william.w.fisher@gmail.com>
2021-10-04 14:29:43 -0700

    Bump version.

--- shellous/__init__.py
+++ shellous/__init__.py
@@ -3,7 +3,7 @@
 """
 __docformat__ = "restructuredtext"
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 # pylint: disable=cyclic-import
 from .command import CmdContext, Command, Options  # noqa: F401
