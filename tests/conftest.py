a685c0e7081a1f563c9720749b3733df6f3cfe97
Bill Fisher <william.w.fisher@gmail.com>
2021-11-02 12:29:42 -0700

    Clean up PatchedMultiLoopChildWatcher.

--- tests/conftest.py
+++ tests/conftest.py
@@ -136,37 +136,37 @@ async def _get_children():
     return children
 
 
-def _serialize(func):
-    """Decorator to serialize a non-reentrant signal function.
-    If one client is already in the critical section, set a flag to run the
-    section one more time. Needs work... There is a small chance a retry will
-    be missed. Testing purposes only.
-    """
-
-    lock = threading.Lock()  # Used as atomic test-and-set.
-    retry = False
-
-    @functools.wraps(func)
-    def _decorator(*args, **kwargs):
-        nonlocal retry
-
-        while True:
-            if lock.acquire(blocking=False):  # pylint: disable=consider-using-with
-                try:
-                    retry = False
-                    func(*args, **kwargs)
-                finally:
-                    lock.release()
-                if retry:
-                    continue
-            else:
-                retry = True
-            break
-
-    return _decorator
-
-
 if sys.platform != "win32":
+
+    def _serialize(func):
+        """Decorator to serialize a non-reentrant signal function.
+        If one client is already in the critical section, set a flag to run the
+        section one more time. Testing purposes only.
+        """
+
+        lock = threading.Lock()  # Used as atomic test-and-set.
+        retry = False
+
+        @functools.wraps(func)
+        def _decorator(*args, **kwargs):
+            nonlocal retry
+
+            while True:
+                if lock.acquire(blocking=False):  # pylint: disable=consider-using-with
+                    try:
+                        retry = False
+                        func(*args, **kwargs)
+                    finally:
+                        lock.release()
+                    if retry:
+                        continue
+                else:
+                    # A signal handler that interrupts an existing handler will
+                    # run to completion (LIFO).
+                    retry = True
+                break
+
+        return _decorator
 
     class PatchedMultiLoopChildWatcher(asyncio.MultiLoopChildWatcher):
         "Test race condition fixes in MultiLoopChildWatcher."
