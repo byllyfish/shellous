5ed285ad3297a24b5610d039d15d073d168dceb5
Bill Fisher <william.w.fisher@gmail.com>
2021-11-07 13:16:05 -0700

    Fix leak of pty_fds.

--- shellous/runner.py
+++ shellous/runner.py
@@ -484,6 +484,8 @@ class Runner:
         try:
             return await self._start()
         except BaseException as ex:
+            if self._options.pty_fds:
+                self._options.pty_fds.close()
             self._stop_timer()  # failsafe just in case
             self._audit_callback("stop", failure=type(ex).__name__)
             raise
