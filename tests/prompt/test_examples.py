795cb26b6cb0a567e5ff0876621664983144958a
Bill Fisher <william.w.fisher@gmail.com>
2023-10-14 12:29:27 -0700

    Fix tests for alpine.

--- tests/prompt/test_examples.py
+++ tests/prompt/test_examples.py
@@ -14,5 +14,11 @@ _PY = sh(sys.executable).env(PYTHONPATH=_DIR.parents[1])
 
 async def test_example1():
     "Test the example1 program."
-    output = await _PY(_DIR / "example1.py")
-    assert output == "arbitrary\r\nYou typed: arbitrary\r\n\n"
+    err = bytearray()
+
+    result = await _PY.result(_DIR / "example1.py").set(timeout=10.0).stderr(err)
+    if result:
+        print(err.decode())
+
+    assert result
+    assert result.output == "arbitrary\r\nYou typed: arbitrary\r\n\n"
