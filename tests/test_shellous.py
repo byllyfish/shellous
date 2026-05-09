5c9ab10e11f33a4a6a2ecd858557686f54457e4c
Bill Fisher <william.w.fisher@gmail.com>
2026-05-09 12:52:33 -0700

    Add test for `limit` option.

--- tests/test_shellous.py
+++ tests/test_shellous.py
@@ -1529,6 +1529,20 @@ async def test_bulk_line_limit(bulk_cmd):
             assert False  # never reached
 
 
+async def test_readline_limit_decreased(echo_cmd):
+    "Test line iteration with artifically lowered `read_buffer_limit`."
+    cmd1 = echo_cmd("x" * 25).set(limit=30)
+    assert [x async for x in cmd1] == ["x" * 25]
+
+
+async def test_readline_limit_too_small(echo_cmd):
+    "Test line iteration with `read_buffer_limit` too small."
+    cmd2 = echo_cmd("y" * 25).set(limit=24)
+    with pytest.raises(ValueError, match="Separator is not found"):
+        async for _ in cmd2:
+            assert False  # never reached
+
+
 def _run(cmd):
     "Run command in process pool executor."
 
